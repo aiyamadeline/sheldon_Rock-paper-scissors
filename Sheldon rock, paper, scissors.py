bc991602c97db3d98c85340a6ccaf887689800cb
aiyamadeline <aiyaedwards123@gmail.com>
2022-11-01 19:37:25 +0000

    rockpaperscissors rockpaperscissors

---
+++ Sheldon rock, paper, scissors.py
@@ -0,0 +1,76 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+options = ["rock", "paper", "scissors", "spock", "lizard"]
+options 
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors of Q to quit: ").lower()
+    if user_input == "q" :
+        break
+
+    if user_input not in options:
+        continue
+
+    random_number = (random.randint(0,4))
+    # rock: 0, paper: 1, scissors: 2, spock: 3, lizard: 4
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+
+
+    elif user_input == "rock" and computer_pick == "lizard":
+        print("You won!")
+        user_wins += 1
+        
+    
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+        
+    
+    elif user_input == "paper" and computer_pick == "spock":
+        print("You won!")
+        user_wins += 1
+
+    
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+        
+    
+    elif user_input == "scissors" and computer_pick == "lizard":
+        print("You won!")
+        user_wins += 1
+    
+   
+    elif user_input == "lizard" and computer_pick == "spock":
+        print("You won!")
+        user_wins += 1
+        
+
+    elif user_input == "lizard" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+
+    
+    elif user_input == "spock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+    
+    
+    elif user_input == "spock" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+    
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("You won", user_wins ,"times")
+print("computer won", computer_wins ,"times") 
